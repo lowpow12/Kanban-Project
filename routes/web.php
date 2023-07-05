49b460e3faf8a595d886c4c4219bb08fc737474c
lowpow12 <distinctlow@gmail.com>
2023-07-05 19:43:26 +0700

    Develop Edit Task View And Create Task View

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('home'); // name ditambahkan
+})->name('home'); 
 
-Route::get('/tasks/', [TaskController::class, 'index'])->name('tasks.index'); // name ditambahkan
+Route::prefix('tasks')
+    ->name('tasks.')
+    ->controller(TaskController::class)
+    ->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('{id}/edit', 'edit')->name('edit');
+        Route::get('/create', 'create')->name('create');
+    });
