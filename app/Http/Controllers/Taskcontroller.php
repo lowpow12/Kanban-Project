0d80fc1af7d106f15666c570e65959f3e67cb842
lowpow12 <distinctlow@gmail.com>
2023-07-12 19:27:08 +0700

    Create task table and access task data through model

--- app/Http/Controllers/Taskcontroller.php
+++ app/Http/Controllers/Taskcontroller.php
@@ -2,64 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    private $tasks;
+   
 
     public function __construct()
     {
-        $this->tasks = [
-            (object) [
-                'id' => 1,
-                'name' => 'Develop Final Project',
-                'detail' => 'Kanban project using PHP and Laravel',
-                'due_date' => '2023-04-30',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 2,
-                'name' => 'Lunch with Guru Domba',
-                'detail' => 'Have Nasi Padang with Guru Domba',
-                'due_date' => '2023-04-10',
-                'status' => 'not_started',
-            ],
-            (object) [
-                'id' => 3,
-                'name' => 'Learn Blade Templating',
-                'detail' => 'Complete Blade Templating material on Progate',
-                'due_date' => '2023-04-05',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 4,
-                'name' => 'Decide Plans for Lebaran holidays',
-                'detail' => 'Trip with family?',
-                'due_date' => '2023-04-21',
-                'status' => 'in_progress',
-            ],
-            (object) [
-                'id' => 5,
-                'name' => 'Develop a Laravel Project',
-                'detail' => 'Develop a Kanban app and ask Guru Domba\'s review',
-                'due_date' => '2023-04-30',
-                'status' => 'in_review',
-            ],
-            (object) [
-                'id' => 6,
-                'name' => 'Learn PHP Basics',
-                'detail' => 'Complete PHP materials on Frontend Course',
-                'due_date' => '2023-04-30',
-                'status' => 'completed',
-            ],
-        ];
+       
     }
 
     public function edit($id)
     {
         $pageTitle = 'Edit Task';
-        $tasks = $this->tasks;
+        $tasks = Task::find($id);
 
         $task = $tasks[$id - 1];
 
@@ -71,10 +29,22 @@ class TaskController extends Controller
         return view('tasks.create', ['pageTitle' => $pageTitle]);
     }
 
+    public function store(Request $request)
+    {
+        Task::create([
+            'name' => $request->name,
+            'detail' => $request->detail,
+            'due_date' => $request->due_date,
+            'status' => $request->status,
+        ]);
+
+        return redirect()->route('tasks.index');
+    }
+
     public function index()
     {
         $pageTitle = 'Task List'; 
-        $tasks = $this->tasks;
+        $tasks = Task::all();
         return view('tasks.index', [
             'pageTitle' => $pageTitle, 
             'tasks' => $tasks,
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,32 +5,33 @@
 @section('main')
 <div class="form-container">
   <h1 class="form-title">{{ $pageTitle }}</h1>
-  <form class="form">
-    <div class="form-item">
+  <form class="form" method="POST" action="{{ route('tasks.store') }}">
+  @csrf  
+  <div class="form-item">
       <label>Name:</label>
-      <input class="form-input" type="text" value="" >
+      <input class="form-input" type="text" value="" name="name">
     </div>
 
     <div class="form-item">
       <label>Detail:</label>
-      <textarea class="form-text-area"></textarea>
+      <textarea class="form-text-area" name="detail"></textarea>
     </div>
 
     <div class="form-item">
       <label>Due Date:</label>
-      <input class="form-input" type="date" value="" >
+      <input class="form-input" type="date" value="" name="due_date">
     </div>
 
     <div class="form-item">
       <label>Progress:</label>
-      <select class="form-input">
+      <select class="form-input" name="status">
         <option value="not_started">Not Started</option>
         <option value="in_progress">In Progress</option>
         <option value="in_review">Waiting/In Review</option>
         <option value="completed">Completed</option>
       </select>
     </div>
-    <button type="button" class="form-button">Submit</button>
+    <button type="submit" class="form-button">Submit</button>
   </form>
 </div>
 @endsection
